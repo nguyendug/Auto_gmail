ae5d6e530367b23a9b56ca20d0b64d7fe5ab77de
Dung ONEADX <ngdung7501@gmail.com>
2023-07-19 18:38:18 +0700

    Xóa Folder có file

--- cookies.js
+++ cookies.js
@@ -2,7 +2,9 @@ const puppeteer = require("puppeteer-core");
 // const { scrollPageToBottom } = require("puppeteer-autoscroll-down");
 // const { scrollPageToTop } = require("puppeteer-autoscroll-down");
 const assert = require("assert");
-const fs = require("fs").promises;
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
 const hidemyacc = new (require("./hidemyacc"))();
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -29,9 +31,14 @@ const waitForSelector = async (page, selector, options = { timeout: 3000 }) => {
   }
 };
 
+const hidemyaccCachePath = path.join(
+  os.homedir(),
+  ".hidemyacc",
+  "cached-profiles"
+);
+
 !(async () => {
   try {
-    // const profileId = "64b64bd2f25829e1fdcea7f9";
     const proxy = {
       mode: "none",
       host: "pr.lunaproxy.com",
@@ -39,19 +46,21 @@ const waitForSelector = async (page, selector, options = { timeout: 3000 }) => {
       username: "user-lu2769084-region-us",
       password: "Abcd!22345",
     };
-    // const success = await hidemyacc.start(profileId, {
-    //   proxy: JSON.stringify(proxy),
-    // });
 
     //set cookies
     try {
+      // Tạo 1 profile mới
       const createProfile = await hidemyacc.create({
         name: "Test1",
         folder: "64b64fa2f25829e1fdd1e142",
       });
       console.log(createProfile.data);
+      //console.log(1);
+      const profileID = createProfile.data.id; //Lấy profile ID vừa tạo
+      console.log(profileID);
 
-      const startProfile = await hidemyacc.start(createProfile.data.id, {
+      //Start profile
+      const startProfile = await hidemyacc.start(profileID, {
         proxy: JSON.stringify(proxy),
       });
 
@@ -61,16 +70,66 @@ const waitForSelector = async (page, selector, options = { timeout: 3000 }) => {
         slowMo: 60,
       });
 
-      const page = await browser.newPage();
-      const cookiesString = await fs.readFile("./cookies.txt");
-      const cookies = JSON.parse(cookiesString);
+      const page = await browser.newPage(); //Mở 1 tab mới
+      const cookiesString = await fs.readFileSync("./cookies.txt"); //Đọc file cookies
+      const cookies = JSON.parse(cookiesString); //Chuyển chuỗi sang dạng opject
       await page.setCookie(...cookies);
       //console.log(cookies);
-      assert(await goto(page, "https://gmail.com/", { waitUntil: "load" }));
+      assert(await goto(page, "https://gmail.com/", { waitUntil: "load" })); //Truy cập gmail và chờ load trang
 
       assert(browser);
-      await delay(5000);
+      await delay(120000);
       await browser.close();
+
+      await delay(5000);
+      //Nối chuỗi
+      const profileCachePath = path.join(hidemyaccCachePath, profileID);
+      console.log(profileCachePath);
+      //Kiểm tra folder có tồn tại và xóa folder có file
+      if (fs.existsSync(profileCachePath)) {
+        fs.rmSync(profileCachePath, { recursive: true, force: true });
+        console.log("Pass");
+      } else {
+        console.log("Delete folder fail");
+      }
+      //console.log(1);
+
+      await delay(10000);
+      const startProfile2 = await hidemyacc.start(profileID, {
+        proxy: JSON.stringify(proxy),
+      });
+      console.log(profileID);
+      const browser2 = await puppeteer.connect({
+        browserWSEndpoint: startProfile2.data.wsUrl,
+        defaultViewport: null,
+        slowMo: 60,
+      });
+      console.log(1);
+      const page2 = await browser2.newPage();
+      assert(await goto(page2, "https://gmail.com/", { waitUntil: "load" }));
+      //console.log(2);
+
+      // await delay(5000);
+      // await browser.newPage();
+      // assert(await goto(page, "https://gmail.com/", { waitUntil: "load" }));
+
+      // assert(await goto(page, "https://gmail.com/", { waitUntil: "load" }));
+      // await page.type(
+      //   'input[aria-label="Email or phone"]',
+      //   "dung.oneadx@gmail.com",
+      //   { waitUntil: "load" }
+      // );
+      // await delay(500);
+      // await page.keyboard.press("Enter", { waitUntil: "load" });
+      // await page.type(
+      //   'aria-label="Enter your password"',
+      //   "maibennhaubannhe75!",
+      //   { waitUntil: "load" }
+      // );
+      // await delay(500);
+      // await page.keyboard.press("Enter", { waitUntil: "load" });
+      // await page.type('input[id="m_login_password"]', "maibennhaubannhe7501!");
+      // await delay(500);
     } catch (error) {
       console.log(error.message);
     }
